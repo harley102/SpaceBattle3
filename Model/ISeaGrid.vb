29734f00b82cf62dd3f91c7ace7a92f7ed7c9a20
Bert <54125860+102568830@users.noreply.github.com>
2019-09-05 11:31:30 +1000

    Converted to C#

--- Model/ISeaGrid.vb
+++ Model/ISeaGrid.vb
@@ -1,32 +1,45 @@
-''' <summary>
-''' The ISeaGrid defines the read only interface of a Grid. This
-''' allows each player to see and attack their opponents grid.
-''' </summary>
-Public Interface ISeaGrid
+/// <summary>
+/// ''' The ISeaGrid defines the read only interface of a Grid. This
+/// ''' allows each player to see and attack their opponents grid.
+/// ''' </summary>
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic;
 
-    ReadOnly Property Width() As Integer
+public interface ISeaGrid
+{
+    int Width { get; }
 
-    ReadOnly Property Height() As Integer
+    int Height { get; }
 
-    ''' <summary>
-    ''' Indicates that the grid has changed.
-    ''' </summary>
-    Event Changed As EventHandler
+    /// <summary>
+    ///     ''' Indicates that the grid has changed.
+    ///     ''' </summary>
+    event EventHandler Changed;
 
-    ''' <summary>
-    ''' Provides access to the given row/column
-    ''' </summary>
-    ''' <param name="row">the row to access</param>
-    ''' <param name="column">the column to access</param>
-    ''' <value>what the player can see at that location</value>
-    ''' <returns>what the player can see at that location</returns>
-    ReadOnly Property Item(ByVal row As Integer, ByVal column As Integer) As TileView
+    /// <summary>
+    ///     ''' Provides access to the given row/column
+    ///     ''' </summary>
+    ///     ''' <param name="row">the row to access</param>
+    ///     ''' <param name="column">the column to access</param>
+    ///     ''' <value>what the player can see at that location</value>
+    ///     ''' <returns>what the player can see at that location</returns>
+    TileView Item { get; }
 
-    ''' <summary>
-    ''' Mark the indicated tile as shot.
-    ''' </summary>
-    ''' <param name="row">the row of the tile</param>
-    ''' <param name="col">the column of the tile</param>
-    ''' <returns>the result of the attack</returns>
-    Function HitTile(ByVal row As Integer, ByVal col As Integer) As AttackResult
-End Interface
+    /// <summary>
+    ///     ''' Mark the indicated tile as shot.
+    ///     ''' </summary>
+    ///     ''' <param name="row">the row of the tile</param>
+    ///     ''' <param name="col">the column of the tile</param>
+    ///     ''' <returns>the result of the attack</returns>
+    AttackResult HitTile(int row, int col);
+}
