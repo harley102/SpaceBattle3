93a09d5c67cfe70ba18eb39008fc1c62ba7ffe14
Bert <54125860+102568830@users.noreply.github.com>
2019-09-05 11:30:42 +1000

    Converted to C#

--- Model/Direction.vb
+++ Model/Direction.vb
@@ -1,14 +1,12 @@
-''' <summary>
-''' The direction the ship can be oriented.
-''' </summary>
-Public Enum Direction
-    ''' <summary>
-    ''' The ship is oriented left/right
-    ''' </summary>
-    LeftRight
+public enum Direction
+{
+    /// <summary>
+    ///     ''' The ship is oriented left/right
+    ///     ''' </summary>
+    LeftRight,
 
-    ''' <summary>
-    ''' The ship is oriented up/down
-    ''' </summary>
+    /// <summary>
+    ///     ''' The ship is oriented up/down
+    ///     ''' </summary>
     UpDown
-End Enum
+}
