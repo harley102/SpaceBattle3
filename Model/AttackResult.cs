0fa245f2fb99eedb392e9a82f39a0b82ed55aaa7
harley102 <54125520+harley102@users.noreply.github.com>
2019-09-08 20:52:28 +1000

    Fix Comments Documentation

--- Model/AttackResult.cs
+++ Model/AttackResult.cs
@@ -1,6 +1,6 @@
 /// <summary>
-/// ''' AttackResult gives the result after a shot has been made.
-/// ''' </summary>
+/// AttackResult gives the result after a shot has been made.
+/// </summary>
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,10 +23,10 @@ public class AttackResult
     private int _Column;
 
     /// <summary>
-    ///     ''' The result of the attack
-    ///     ''' </summary>
-    ///     ''' <value>The result of the attack</value>
-    ///     ''' <returns>The result of the attack</returns>
+    /// The result of the attack
+    /// </summary>
+    /// <value>The result of the attack</value>
+    /// <returns>The result of the attack</returns>
     public ResultOfAttack Value
     {
         get
@@ -36,10 +36,10 @@ public class AttackResult
     }
 
     /// <summary>
-    ///     ''' The ship, if any, involved in this result
-    ///     ''' </summary>
-    ///     ''' <value>The ship, if any, involved in this result</value>
-    ///     ''' <returns>The ship, if any, involved in this result</returns>
+    /// The ship, if any, involved in this result
+    /// </summary>
+    /// <value>The ship, if any, involved in this result</value>
+    /// <returns>The ship, if any, involved in this result</returns>
     public Ship Ship
     {
         get
@@ -49,11 +49,11 @@ public class AttackResult
     }
 
     /// <summary>
-    ///     ''' A textual description of the result.
-    ///     ''' </summary>
-    ///     ''' <value>A textual description of the result.</value>
-    ///     ''' <returns>A textual description of the result.</returns>
-    ///     ''' <remarks>A textual description of the result.</remarks>
+    /// A textual description of the result.
+    /// </summary>
+    /// <value>A textual description of the result.</value>
+    /// <returns>A textual description of the result.</returns>
+    /// <remarks>A textual description of the result.</remarks>
     public string Text
     {
         get
@@ -63,8 +63,8 @@ public class AttackResult
     }
 
     /// <summary>
-    ///     ''' The row where the attack occurred
-    ///     ''' </summary>
+    /// The row where the attack occurred
+    /// </summary>
     public int Row
     {
         get
@@ -74,8 +74,8 @@ public class AttackResult
     }
 
     /// <summary>
-    ///     ''' The column where the attack occurred
-    ///     ''' </summary>
+    /// The column where the attack occurred
+    /// </summary>
     public int Column
     {
         get
@@ -85,9 +85,9 @@ public class AttackResult
     }
 
     /// <summary>
-    ///     ''' Set the _Value to the PossibleAttack value
-    ///     ''' </summary>
-    ///     ''' <param name="value">either hit, miss, destroyed, shotalready</param>
+    /// Set the _Value to the PossibleAttack value
+    /// </summary>
+    /// <param name="value">either hit, miss, destroyed, shotalready</param>
     public AttackResult(ResultOfAttack value, string text, int row, int column)
     {
         _Value = value;
@@ -98,19 +98,19 @@ public class AttackResult
     }
 
     /// <summary>
-    ///     ''' Set the _Value to the PossibleAttack value, and the _Ship to the ship
-    ///     ''' </summary>
-    ///     ''' <param name="value">either hit, miss, destroyed, shotalready</param>
-    ///     ''' <param name="ship">the ship information</param>
+    /// Set the _Value to the PossibleAttack value, and the _Ship to the ship
+    /// </summary>
+    /// <param name="value">either hit, miss, destroyed, shotalready</param>
+    /// <param name="ship">the ship information</param>
     public AttackResult(ResultOfAttack value, Ship ship, string text, int row, int column) : this(value, text, row, column)
     {
         _Ship = ship;
     }
 
     /// <summary>
-    ///     ''' Displays the textual information about the attack
-    ///     ''' </summary>
-    ///     ''' <returns>The textual information about the attack</returns>
+    /// Displays the textual information about the attack
+    /// </summary>
+    /// <returns>The textual information about the attack</returns>
     public override string ToString()
     {
         if (_Ship == null)
