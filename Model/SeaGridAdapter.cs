7765c1cd960ef3e565c073bb1fbba7a247ff49eb
harley102 <54125520+harley102@users.noreply.github.com>
2019-09-08 21:00:01 +1000

    Fix Comments Documentation

--- Model/SeaGridAdapter.cs
+++ Model/SeaGridAdapter.cs
@@ -1,7 +1,7 @@
 /// <summary>
-/// ''' The SeaGridAdapter allows for the change in a sea grid view. Whenever a ship is
-/// ''' presented it changes the view into a sea tile instead of a ship tile.
-/// ''' </summary>
+/// The SeaGridAdapter allows for the change in a sea grid view. Whenever a ship is
+/// presented it changes the view into a sea tile instead of a ship tile.
+/// </summary>
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -20,9 +20,9 @@ public class SeaGridAdapter : ISeaGrid
     private SeaGrid _MyGrid;
 
     /// <summary>
-    ///     ''' Create the SeaGridAdapter, with the grid, and it will allow it to be changed
-    ///     ''' </summary>
-    ///     ''' <param name="grid">the grid that needs to be adapted</param>
+    /// Create the SeaGridAdapter, with the grid, and it will allow it to be changed
+    /// </summary>
+    /// <param name="grid">the grid that needs to be adapted</param>
     public SeaGridAdapter(SeaGrid grid)
     {
         _MyGrid = grid;
@@ -30,10 +30,10 @@ public class SeaGridAdapter : ISeaGrid
     }
 
     /// <summary>
-    ///     ''' MyGrid_Changed causes the grid to be redrawn by raising a changed event
-    ///     ''' </summary>
-    ///     ''' <param name="sender">the object that caused the change</param>
-    ///     ''' <param name="e">what needs to be redrawn</param>
+    /// MyGrid_Changed causes the grid to be redrawn by raising a changed event
+    /// </summary>
+    /// <param name="sender">the object that caused the change</param>
+    /// <param name="e">what needs to be redrawn</param>
     private void MyGrid_Changed(object sender, EventArgs e)
     {
         Changed?.Invoke(this, e);
@@ -41,11 +41,11 @@ public class SeaGridAdapter : ISeaGrid
 
 
     /// <summary>
-    ///     ''' Changes the discovery grid. Where there is a ship we will sea water
-    ///     ''' </summary>
-    ///     ''' <param name="x">tile x coordinate</param>
-    ///     ''' <param name="y">tile y coordinate</param>
-    ///     ''' <returns>a tile, either what it actually is, or if it was a ship then return a sea tile</returns>
+    /// Changes the discovery grid. Where there is a ship we will sea water
+    /// </summary>
+    /// <param name="x">tile x coordinate</param>
+    /// <param name="y">tile y coordinate</param>
+    /// <returns>a tile, either what it actually is, or if it was a ship then return a sea tile</returns>
     public TileView Item
     {
         get
@@ -60,13 +60,13 @@ public class SeaGridAdapter : ISeaGrid
     }
 
     /// <summary>
-    ///     ''' Indicates that the grid has been changed
-    ///     ''' </summary>
+    /// Indicates that the grid has been changed
+    /// </summary>
     public event EventHandler Changed;
 
     /// <summary>
-    ///     ''' Get the width of a tile
-    ///     ''' </summary>
+    ///  Get the width of a tile
+    /// </summary>
     public int Width
     {
         get
@@ -76,8 +76,8 @@ public class SeaGridAdapter : ISeaGrid
     }
 
     /// <summary>
-    ///     ''' Get the height of the tile
-    ///     ''' </summary>
+    /// Get the height of the tile
+    /// </summary>
     public int Height
     {
         get
@@ -87,11 +87,11 @@ public class SeaGridAdapter : ISeaGrid
     }
 
     /// <summary>
-    ///     ''' HitTile calls oppon _MyGrid to hit a tile at the row, col
-    ///     ''' </summary>
-    ///     ''' <param name="row">the row its hitting at</param>
-    ///     ''' <param name="col">the column its hitting at</param>
-    ///     ''' <returns>The result from hitting that tile</returns>
+    /// HitTile calls oppon _MyGrid to hit a tile at the row, col
+    /// </summary>
+    /// <param name="row">the row its hitting at</param>
+    /// <param name="col">the column its hitting at</param>
+    /// <returns>The result from hitting that tile</returns>
     public AttackResult HitTile(int row, int col)
     {
         return _MyGrid.HitTile(row, col);
