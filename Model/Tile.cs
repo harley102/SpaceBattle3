fd4112f0d4dc4a5d048d7a5f125b099822570696
harley102 <54125520+harley102@users.noreply.github.com>
2019-09-08 20:56:22 +1000

    Fix Comments Documentation

--- Model/Tile.cs
+++ Model/Tile.cs
@@ -1,7 +1,7 @@
 /// <summary>
-/// ''' Tile knows its location on the grid, if it is a ship and if it has been 
-/// ''' shot before
-/// ''' </summary>
+/// Tile knows its location on the grid, if it is a ship and if it has been 
+/// shot before
+/// </summary>
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -23,10 +23,10 @@ public class Tile
     private bool _Shot = false;    // the tile has been shot at
 
     /// <summary>
-    ///     ''' Has the tile been shot?
-    ///     ''' </summary>
-    ///     ''' <value>indicate if the tile has been shot</value>
-    ///     ''' <returns>true if the tile was shot</returns>
+    /// Has the tile been shot?
+    /// </summary>
+    /// <value>indicate if the tile has been shot</value>
+    /// <returns>true if the tile was shot</returns>
     public bool Shot
     {
         get
@@ -40,10 +40,10 @@ public class Tile
     }
 
     /// <summary>
-    ///     ''' The row of the tile in the grid
-    ///     ''' </summary>
-    ///     ''' <value>the row index of the tile in the grid</value>
-    ///     ''' <returns>the row index of the tile</returns>
+    /// The row of the tile in the grid
+    /// </summary>
+    /// <value>the row index of the tile in the grid</value>
+    /// <returns>the row index of the tile</returns>
     public int Row
     {
         get
@@ -53,10 +53,10 @@ public class Tile
     }
 
     /// <summary>
-    ///     ''' The column of the tile in the grid
-    ///     ''' </summary>
-    ///     ''' <value>the column of the tile in the grid</value>
-    ///     ''' <returns>the column of the tile in the grid</returns>
+    /// The column of the tile in the grid
+    /// </summary>
+    /// <value>the column of the tile in the grid</value>
+    /// <returns>the column of the tile in the grid</returns>
     public int Column
     {
         get
@@ -66,8 +66,8 @@ public class Tile
     }
 
     /// <summary>
-    ///     ''' Ship allows for a tile to check if there is ship and add a ship to a tile
-    ///     ''' </summary>
+    /// Ship allows for a tile to check if there is ship and add a ship to a tile
+    /// </summary>
     public Ship Ship
     {
         get
@@ -88,11 +88,11 @@ public class Tile
     }
 
     /// <summary>
-    ///     ''' The tile constructor will know where it is on the grid, and is its a ship
-    ///     ''' </summary>
-    ///     ''' <param name="row">the row on the grid</param>
-    ///     ''' <param name="col">the col on the grid</param>
-    ///     ''' <param name="ship">what ship it is</param>
+    /// The tile constructor will know where it is on the grid, and is its a ship
+    /// </summary>
+    /// <param name="row">the row on the grid</param>
+    /// <param name="col">the col on the grid</param>
+    /// <param name="ship">what ship it is</param>
     public Tile(int row, int col, Ship ship)
     {
         _RowValue = row;
@@ -101,16 +101,16 @@ public class Tile
     }
 
     /// <summary>
-    ///     ''' Clearship will remove the ship from the tile
-    ///     ''' </summary>
+    /// Clearship will remove the ship from the tile
+    /// </summary>
     public void ClearShip()
     {
         _Ship = null;
     }
 
     /// <summary>
-    ///     ''' View is able to tell the grid what the tile is
-    ///     ''' </summary>
+    /// View is able to tell the grid what the tile is
+    /// </summary>
     public TileView View
     {
         get
@@ -136,9 +136,9 @@ public class Tile
     }
 
     /// <summary>
-    ///     ''' Shoot allows a tile to be shot at, and if the tile has been hit before
-    ///     ''' it will give an error
-    ///     ''' </summary>
+    /// Shoot allows a tile to be shot at, and if the tile has been hit before
+    /// it will give an error
+    /// </summary>
     internal void Shoot()
     {
         if ((false == Shot))
